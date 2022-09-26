2a074b8aa4563354eeed50c23f7a4a9894d16b4d
barjenbruchlaw <brian@kcpropertylaw.com>
2022-09-26 14:19:56 -0500

    Our Questions 08:50

--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,16 +1,9 @@
 <script>
+  import { dataset_dev } from "svelte/internal"
   import { get } from "svelte/store"
+  import Question from "./Question.svelte"
 
   let quiz = getQuiz()
-  let result
-  let correctAnswer = "b"
-  let answers = ["a", "b", "c", "d"]
-  let pickAnswer = (answer) => {
-    if (answer == correctAnswer) {
-      return (result = "Correct!")
-    }
-    return (result = "Oops!")
-  }
   async function getQuiz() {
     const res = await fetch(
       "https://opentdb.com/api.php?amount=10&category=24&type=multiple"
@@ -26,27 +19,14 @@
 <div>
   <button on:click={handleClick}>Get Questions</button>
 
-  {#if result}
-    <h4>{result}</h4>
-  {:else}
-    <h5>Please pick an answer</h5>
-  {/if}
-
   {#await quiz}
     Loading ...
   {:then data}
-    <h3>{data.results[0].question}</h3>
+    {#each data.results as question}
+      <Question {question} />
+    {/each}
   {/await}
-
-  {#each answers as answer}
-    <button on:click={() => pickAnswer(answer)}
-      >Answer {answer.toUpperCase()}</button
-    >
-  {/each}
 </div>
 
 <style>
-  h4 {
-    color: red;
-  }
 </style>
